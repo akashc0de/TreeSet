2a1ed1e8e4ba5c1015c3bb0e45b5ff52aca70ad0
Akash <111621704+akashc0de@users.noreply.github.com>
2022-09-03 15:36:36 +0530

    Add files via upload

---
+++ treeSet.java
@@ -0,0 +1,21 @@
+package colllectionLearning;
+
+import java.util.TreeSet;
+
+public class treeSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet t=new TreeSet();
+		t.add(5);
+		t.add(20);
+		t.add(4);
+		t.add(7);
+		t.add(8);
+		t.add(5);
+		System.out.println(t);
+		
+
+	}
+
+}
